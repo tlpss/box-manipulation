f5daab40331cf2869dd248c069addf23bcc527f4
tlips <thomas.lips@ugent.be>
2021-11-05 21:49:52 +0000

    train on GPU using pl device management

--- keypoint-detection/src/models.py
+++ keypoint-detection/src/models.py
@@ -6,8 +6,7 @@ from src.keypoint_utils import generate_keypoints_heatmap
 
 
 class KeypointDetector(pl.LightningModule):
-    # TODO: REFACTOR
-    # TODO: fix issues with CUDA devices (data not consistenly on right device)
+    # TODO: REFACTOR & CLEAN
     """
     Box keypoint Detector using Gaussian Heatmaps
     There are 2 channels (groups) of keypoints, one that detect box corners and once that detects the center of the outer edge of all flaps of the box.
@@ -16,8 +15,9 @@ class KeypointDetector(pl.LightningModule):
 
     def __init__(self, heatmap_sigma=10):
         super().__init__()
+        ## No need to manage devices ourselves, pytorch.lightning does all of that.
+        ## device can be accessed through self.device if required.
         # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        # self.device_ = device
 
         self.heatmap_sigma = heatmap_sigma
 
@@ -86,20 +86,24 @@ class KeypointDetector(pl.LightningModule):
         return torch.nn.functional.binary_cross_entropy(predicted_heatmaps, heatmaps, reduction="mean")
 
     def create_heatmap_batch(self, shape, keypoints):
+        # TODO: profile to see if the conversion from and to GPU does not introduce a bottleneck
+        # alternative is to create heatmaps on GPU by passing device to the generate_keypoints_heatmap function
+
+        # convert keypoints to cpu to create the heatmaps
         batch_heatmaps = [
-            generate_keypoints_heatmap(shape, keypoints[i], self.heatmap_sigma) for i in range(len(keypoints))
+            generate_keypoints_heatmap(shape, keypoints[i].cpu(), self.heatmap_sigma) for i in range(len(keypoints))
         ]
         batch_heatmaps = np.stack(batch_heatmaps, axis=0)
         batch_heatmaps = torch.from_numpy(batch_heatmaps)
-        return batch_heatmaps
+        return batch_heatmaps.to(self.device)
 
     def training_step(self, train_batch, batch_idx):
         imgs, corner_keypoints, flap_keypoints = train_batch
         # load here to device to keep mem consumption low, if possible one could also load entire dataset on GPU to speed up training..
         imgs = imgs.to(self.device)
         # create heatmaps JIT, is this desirable?
-        corner_heatmaps = self.create_heatmap_batch(imgs[0].shape[1:], corner_keypoints).to(self.device)
-        # flap_heatmaps = self.create_heatmap_batch(img[0].shape[1:], flap_keypoints).to(self.device)
+        corner_heatmaps = self.create_heatmap_batch(imgs[0].shape[1:], corner_keypoints)
+        # flap_heatmaps = self.create_heatmap_batch(img[0].shape[1:], flap_keypoints)
 
         predicted_heatmaps = self.forward(imgs)
         predicted_corner_heatmaps = predicted_heatmaps[:, 0, :, :]
