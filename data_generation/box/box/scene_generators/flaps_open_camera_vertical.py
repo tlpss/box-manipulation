f79577800f8194bccc91a8616fdb6fb129f08e64
victorlouisdg <victorlouisdg@gmail.com>
2022-02-04 11:45:47 +0100

    random box color

--- data_generation/box/box/scene_generators/flaps_open_camera_vertical.py
+++ data_generation/box/box/scene_generators/flaps_open_camera_vertical.py
@@ -6,6 +6,7 @@ import airo_blender_toolkit as abt
 import bpy
 import numpy as np
 from box.box import Box
+from mathutils import Color
 
 os.environ["INSIDE_OF_THE_INTERNAL_BLENDER_PYTHON_ENVIRONMENT"] = "1"
 import blenderproc as bproc
@@ -42,6 +43,14 @@ def generate_scene(seed):
     box.set_rotation_euler([0, 0, np.random.uniform(0.0, 2 * np.pi)])
     box.set_location((0, 0, 0.001))
     box.persist_transformation_into_mesh()
+
+    box_color = Color()
+    box_hue = np.random.uniform(0, 0.14)
+    box_saturation = np.random.uniform(0.4, 0.8)
+    box_value = np.random.uniform(0.2, 0.5)
+    box_color.hsv = box_hue, box_saturation, box_value
+    box_material = box.new_material("box")
+    box_material.set_principled_shader_value("Base Color", tuple(box_color) + (1,))
 
     camera = bpy.context.scene.camera
     camera.location = (0, 0, 1.25)
