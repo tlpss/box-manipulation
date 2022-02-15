8537efeb09a93ab44d4c55334e3dc801bbb74461
tlips <thomas17.lips@gmail.com>
2022-02-15 08:58:47 +0000

    add keypoint detector as submodule

--- keypoint_detection/setup.sh
+++ keypoint_detection/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+###################################################
+##script to use for creating env in a Job request##
+###################################################
+
 # move to user home dir
 cd ~
 # activate conda env
@@ -11,8 +15,8 @@ if [[ -d ~/box-manipulation ]]; then
     echo "removing box dir"
     sudo rm -r ~/box-manipulation
 fi
-git clone https://github.com/tlpss/box-manipulation.git
+git clone https://github.com/tlpss/box-manipulation.git git clone --recurse-submodules
 
 # install keypoint_detection package
 # install in dev mode to enable symlinks
-pip install -e  box-manipulation/keypoint_detection
+pip install -e  box-manipulation/keypoint_detection/keypoint_detection
